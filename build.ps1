763ad286d48cbabd8d94667df905c63c19bc3fdf
Ovler <ovlertheyoung@gmail.com>
2024-01-23 22:56:10 +0800

    Remove webpack build file and update build time calculation

--- build.ps1
+++ build.ps1
@@ -1,10 +1,16 @@
 $DIR = "frontend" # 需要追踪的目录
 $BUILD_FILE = ".buildtime" # 用于存储上次构建时间的文件
 
+
+# remove the webpack build file if it exists. it's in dist of the current folder, not the frontend folder
+if (Test-Path "dist\main.js") {
+    Remove-Item "dist\main.js"
+}
+pnpm webpack 
+
+
 # Get the directory's last modified time in ticks
 $dir_mod_time = (Get-Item $DIR).LastWriteTimeUtc.Ticks
-
-pnpm webpack 
 
 if (Test-Path $BUILD_FILE) {
     # Get the last build time
